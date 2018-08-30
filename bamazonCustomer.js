b63487aacadbc094c05317e20f9f66ea2c97c29f
GreenEyedOcelot <hbe123@gmail.com>
2018-08-30 17:56:31 -0400

    bug fixes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,11 +4,20 @@ var inquirer = require("inquirer");
 const cTable = require('console.table');
 var productList = null;
 const THE_HOST = "localhost";
-const THE_USER = "root";
 const THE_DATABASE = "bamazon";
 const THE_USER = "root";
 const THE_PASSWORD = "Scout825";
 const THE_PORT = 3306;
+
+var setHeadings = (obj) => {
+   return {
+      "ID": obj["product_id"],
+      "Product": obj["product_name"],
+      "Department": obj["department_name"],
+      "Price": obj["product_price"],
+      "Available": obj["product_quantity"]
+   };
+};
 
 showTable();
 
@@ -16,37 +25,30 @@ function showTable() {
    var selectStr = "SELECT products.product_id, products.product_name, departments.department_name, products.product_price, products.product_quantity FROM products INNER JOIN departments ON products.department = departments.department_id";
    var connection = mysql.createConnection({
       host: THE_HOST,
-   
+
       // Your port; if not 3306
       port: THE_PORT,
-   
+
       // Your username
       user: THE_USER,
-   
+
       // Your password
       password: THE_PASSWORD,
       database: THE_DATABASE
    });
 
    connection.query(selectStr, function (err, res) {
-      var setHeadings = (obj) => {
-         return {
-            "ID": obj["product_id"],
-            "Product": obj["product_name"],
-            "Department": obj["department_name"],
-            "Price": obj["product_price"],
-            "Available": obj["product_quantity"]
-         };
-      }
+    
       if (err) {
-          throw err;
+         throw err;
       } else {
-         productList = cTable.getTable(res.map(setHeadings));
-         console.log(productList);
+         productList = res.map(setHeadings);
+         var output = cTable.getTable(productList);
+         console.log("\n" + output);
          connection.end();
          promptForPurchase();
       }
-     
+
    });
 }
 
@@ -60,79 +62,77 @@ function promptForPurchase() {
       q <= productList.filter((obj) => obj.ID == id)[0].Available;
    var id = null;
 
-   console.log("Please enter a valid product id.");
+   // console.log("Please enter a valid product id.");
    prompt.get([{
 
-         name: 'id',
-         warning: 'Please enter a valid product id.'
-         empty: false,
-         conform: function (val) {
-            return idIsValid(val);
-         }
-      }], function (err, result) {
-         if (err) {
-            console.log("there is an error when entering an id, it is...", err);
-         } else {
-            id = result.id;
-         }
-      })
-
-      console.log("Please enter a valid product id".);
-      prompt.get([{
-
-         name: 'quantity',
-         warning: 'Please enter a quantity less than or equal to the available units.',
-         empty: false,
-         conform: function (val) {
-            return quantityIsValid(id,val);
-         }
-      }], function (err, result) {
-         if (err) {
-            console.log("there is an error when entering a quantity, it is...", err);
-         } else {
-            console.log("quantity is valid");
-            subtractQuantityAndShowTableAgain(id,quantity);
-         }
-      })
-
-
-function subtractQuantityAndShowTableAgain(quantity) {
-   
-   var selectStr = "UPDATE PRODUCTS SET product_quantity = product_quantity - " + quantity + " WHERE product_id = 5";
-   var connection = mysql.createConnection({
-      host: THE_HOST,
-   
-      // Your port; if not 3306
-      port: THE_PORT,
-   
-      // Your username
-      user: THE_USER,
-   
-      // Your password
-      password: THE_PASSWORD,
-      database: THE_DATABASE
-   });
-
-   connection.query(selectStr, function (err, res) {
-      if (err) {
-          throw err;
-      } else {
-         productList = cTable.getTable(res.map(setHeadings));
-         console.log(productList);
-         connection.end();
-
-         showTable();
+      name: 'id',
+      warning: 'Please enter a valid product id.',
+      empty: false,
+      conform: function (val) {
+         return idIsValid(val);
       }
-     
-   });
+   }], function (err, result) {
+      if (err) {
+         console.log("there is an error when entering an id, it is...", err);
+      } else {
+         id = result.id;
+         prompt.get([{
+
+            name: 'quantity',
+            warning: 'Please enter a quantity less than or equal to the available units.',
+            empty: false,
+            conform: function (val) {
+               return quantityIsValid(id, val);
+            }
+         }], function (err, result) {
+            if (err) {
+               console.log("there is an error when entering a quantity, it is...", err);
+            } else {
+               subtractQuantityAndShowTableAgain(id, result.quantity);
+            }
+         })
+      }
+   })
 
 
-}
 
 
 
+   function subtractQuantityAndShowTableAgain(id, quantity) {
+      var thePrice = parseFloat(productList.filter((obj) => obj.ID == id)[0].Price);
+      var theTotalPrice = parseInt(quantity) * thePrice;
+      console.log("Your total price was " + theTotalPrice);
 
+      var updateStr = "UPDATE PRODUCTS SET product_quantity = product_quantity - " + quantity + " WHERE product_id  = " + id;
+      var connection = mysql.createConnection({
+         host: THE_HOST,
+
+         // Your port; if not 3306
+         port: THE_PORT,
+
+         // Your username
+         user: THE_USER,
+
+         // Your password
+         password: THE_PASSWORD,
+         database: THE_DATABASE
+      });
+
+      connection.query(updateStr, function (err, res) {
+         if (err) {
+            throw err;
+         } else {
+            showTable();
+         }
+
+      });
 
 
    }
 
+
+
+
+
+
+}
